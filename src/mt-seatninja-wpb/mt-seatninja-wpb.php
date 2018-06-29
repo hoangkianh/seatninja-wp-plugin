342378e133abb5ac7b41791df694ff04e2303444
hka <hoangkianh31592@gmail.com>
2018-06-29 08:36:50 +0700

    add plugin metadata

---
+++ src/mt-seatninja-wpb/mt-seatninja-wpb.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: SeatNinja for WPBakery PageBuilder
+ * Plugin URI: https://wpmoNrtar.com/mt-seatninja-wpb
+ * Description: This plugin makes it easy to create reservation pages for your restaurant. All data is taken from seatninja.com. You can use its elements for WPBakery Page Builder, also included 3 themes, or you can customize very easily. You can increase the number of customers of your restaurant with our plugin!
+ * Version: 1.0
+ * Author: WPMortar
+ * Author URI: https://wpmortar.com/
+ * License: GPL-3.0
+ * Domain: mt-snj
+ * Domain Path: /languages
+ */
