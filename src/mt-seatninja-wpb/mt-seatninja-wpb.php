1d1a600acd15403f7dd5bc24667d765963e28b2d
hka <hoangkianh31592@gmail.com>
2018-06-30 19:09:10 +0700

    fix description

--- src/mt-seatninja-wpb/mt-seatninja-wpb.php
+++ src/mt-seatninja-wpb/mt-seatninja-wpb.php
@@ -2,15 +2,19 @@
 /**
  * Plugin Name: SeatNinja for WPBakery PageBuilder
  * Plugin URI: https://wpmortar.com/mt-seatninja-wpb
- * Description: This plugin makes it easy to create reservation pages for your restaurant. All data is taken from
- * seatninja.com. You can use its elements for WPBakery Page Builder, also included 3 themes, or you can customize very
- * easily. You can increase the number of customers of your restaurant with our plugin! Version: 1.0 Author: WPMortar
- * Author URI: https://wpmortar.com/ License: GPL-3.0 Domain: mt-snj Domain Path: /languages
+ * Description: This plugin makes it easy to create reservation pages for your restaurant. All data is taken from seatninja.com. You can use its elements for WPBakery Page Builder, also included 3 themes, or you can customize very easily. You can increase the number of customers of your restaurant with our plugin!
+ * Version: 1.0
+ * Author: WPMortar
+ * Author URI: https://wpmortar.com/
+ * License: GPL-3.0
+ * Domain: mt-snj
+ * Domain Path: /languages
  */
 
 if ( ! class_exists( 'MT_SeatNinja' ) ) {
 
 	class MT_SeatNinja {
+
 	}
 
 	new MT_SeatNinja();
