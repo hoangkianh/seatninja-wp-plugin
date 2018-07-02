b44f939bdde8ba47d7b22c4b4b50cd4a56a3732c
hka <hoangkianh31592@gmail.com>
2018-07-03 06:25:43 +0700

    :art: format code

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,45 @@
 'use strict'
-const gulp = require( 'gulp' )
-const glob = require( 'glob' )
-const bs = require( 'browser-sync' )
-const $ = require( 'gulp-load-plugins' )()
-const mqpacker = require( 'css-mqpacker' )
-const autoprefixer = require( 'autoprefixer' )
-const assets = require( 'postcss-assets' )
-const reportError = require( './report-error' )
-const files = glob( 'src/*', { sync: true } )
-const plugin = files[0].replace( 'src/', '' )
+const gulp = require('gulp')
+const glob = require('glob')
+const bs = require('browser-sync')
+const $ = require('gulp-load-plugins')()
+const mqpacker = require('css-mqpacker')
+const autoprefixer = require('autoprefixer')
+const assets = require('postcss-assets')
+const reportError = require('./report-error')
+const files = glob('src/*', {sync: true})
+const plugin = files[0].replace('src/', '')
 
-gulp.task( 'sass', function() {
-    return gulp.src( 'src/' + plugin + '/assets/scss/*.scss' )
-               .pipe( $.plumber( { errorHandler: reportError } ) )
-               .pipe( $.sourcemaps.init() )
-               .pipe( $.sassGlobImport() )
-               .pipe( $.sass() )
-               .pipe( $.postcss( [autoprefixer( { browsers: ['last 2 versions'] } ), mqpacker( { sort: true } ),
-                                  assets( { loadPaths: ['src/' + plugin + 'assets/images/'] } )] ) )
-               .pipe( $.sourcemaps.write( '../sourcemap/', {
+gulp.task('sass', function () {
+    return gulp.src('src/' + plugin + '/assets/scss/*.scss')
+               .pipe($.plumber({errorHandler: reportError}))
+               .pipe($.sourcemaps.init())
+               .pipe($.sassGlobImport())
+               .pipe($.sass())
+               .pipe($.postcss([autoprefixer({browsers: ['last 2 versions']}), mqpacker({sort: true}),
+                                assets({loadPaths: ['src/' + plugin + 'assets/images/']})]))
+               .pipe($.sourcemaps.write('../sourcemap/', {
                    includeContent: false,
                    sourceRoot    : '../../scss/'
-               } ) )
-               .pipe( $.lineEndingCorrector() )
-               .pipe( gulp.dest( 'src/' + plugin + '/assets/css' ) );
-} )
+               }))
+               .pipe($.lineEndingCorrector())
+               .pipe(gulp.dest('src/' + plugin + '/assets/css'))
+})
 
-gulp.task('bs', function() {
+gulp.task('bs', function () {
     bs.init({
         files: 'src/' + plugin + '/assets/css/*.css'
     })
 })
 
-gulp.task( 'bs-reload', function() {
-    bs.reload();
-} )
+gulp.task('bs-reload', function () {
+    bs.reload()
+})
 
-gulp.task( 'watch', function() {
-    gulp.watch( 'src/' + plugin + '/assets/scss/*.scss', ['sass'] )
-    gulp.watch( 'src/' + plugin + '/assets/js/*.js', ['bs-reload'] )
-    gulp.watch( 'src/' + plugin + '/**/*.php', ['bs-reload'] )
-} )
+gulp.task('watch', function () {
+    gulp.watch('src/' + plugin + '/assets/scss/*.scss', ['sass'])
+    gulp.watch('src/' + plugin + '/assets/js/*.js', ['bs-reload'])
+    gulp.watch('src/' + plugin + '/**/*.php', ['bs-reload'])
+})
 
-gulp.task( 'default', ['bs', 'sass', 'watch'] )
+gulp.task('default', ['bs', 'sass', 'watch'])
--- src/mt-seatninja-wpb/inc/pages/settings.php
+++ src/mt-seatninja-wpb/inc/pages/settings.php
@@ -1,40 +1,43 @@
 <div class="wrap ultimate-icons-wrap">
-	<div class="wrap-container">
-		<div class="ultimate-icons-heading">
-			<h1><?php esc_html_e( 'Seat Ninja for WPBakery Page Builder Settings', 'mt-snj' ) ?></h1>
-			<h3>
-				<?php esc_html_e( 'Congratulations! You are about to use a useful WordPress plugin
+    <div class="wrap-container">
+        <div class="ultimate-icons-heading">
+            <h1><?php esc_html_e( 'Seat Ninja for WPBakery Page Builder Settings', 'mt-snj' ) ?></h1>
+            <h3>
+                <?php esc_html_e( 'Congratulations! You are about to use a useful WordPress plugin
 				that gives you the great ability to get a table booking through seatninja.com.
 				From where ever you are, and then show up when your table is ready and know exactly
-				what you are going to order ahead of time, like a ninja!', 'mt-snj' ) ?>
-			</h3>
-			<div class="mtui-logo">
-			</div>
-		</div>
-		<div class="ultimate-icons-body">
-			<div class="nav-tab-wrapper">
-				<a class="nav-tab nav-tab-active" href="<?php echo esc_url( admin_url( '?page=mt-snj-general' ) ) ?>">
-					<?php esc_html_e( 'General Settings', 'mt-snj' ) ?></a>
-				<a class="nav-tab" href="<?php echo esc_url( admin_url( '?page=mtui-about' ) ) ?>">
-					<?php esc_html_e( 'About', 'mt-snj' ) ?></a>
-			</div>
-			<div class="container tab-content-wrapper">
-				<form method="post" id="mt-snj-settings">
-					<input type="hidden" name="icon-fonts" id="icon-fonts">
-					<table class="form-table">
-						<tr>
-							<th><label for="api-key"><?php esc_html_e( 'Seat Ninja API Key', 'mt-snj' ) ?></label></th>
-							<td><input class="widefat" id="api-key" type="text"/></td>
-						</tr>
-						<tr>
-							<th><label for="customer-token"><?php esc_html_e( 'Customer AuthToken', 'mt-snj' ) ?></label></th>
-							<td><input class="widefat" id="customer-token" type="text"/></td>
-						</tr>
-					</table>
-					<button type="submit" name="submit" id="submit-btn" class="button button-primary">Save Changes</button>
-				</form>
-			</div>
-		</div>
-	</div>
+				what you are going to order ahead of time, like a ninja!',
+                    'mt-snj' ) ?>
+            </h3>
+            <div class="mtui-logo">
+            </div>
+        </div>
+        <div class="ultimate-icons-body">
+            <div class="nav-tab-wrapper">
+                <a class="nav-tab nav-tab-active" href="<?php echo esc_url( admin_url( '?page=mt-snj-general' ) ) ?>">
+                    <?php esc_html_e( 'General Settings', 'mt-snj' ) ?></a>
+                <a class="nav-tab" href="<?php echo esc_url( admin_url( '?page=mtui-about' ) ) ?>">
+                    <?php esc_html_e( 'About', 'mt-snj' ) ?></a>
+            </div>
+            <div class="container tab-content-wrapper">
+                <form method="post" id="mt-snj-settings">
+                    <input type="hidden" name="icon-fonts" id="icon-fonts">
+                    <table class="form-table">
+                        <tr>
+                            <th><label for="api-key"><?php esc_html_e( 'Seat Ninja API Key', 'mt-snj' ) ?></label></th>
+                            <td><input class="widefat" id="api-key" type="text"/></td>
+                        </tr>
+                        <tr>
+                            <th><label for="customer-token"><?php esc_html_e( 'Customer AuthToken',
+                                        'mt-snj' ) ?></label></th>
+                            <td><input class="widefat" id="customer-token" type="text"/></td>
+                        </tr>
+                    </table>
+                    <button type="submit" name="submit" id="submit-btn" class="button button-primary">Save Changes
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
 <?php
