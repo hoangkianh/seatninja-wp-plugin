100dc423eebd64eea7d3a35d1c3ff5ccd8deab7e
hka <hoangkianh31592@gmail.com>
2018-07-08 20:37:16 +0700

    update HTML for mt-seatninja shortcode

--- src/mt-seatninja-wpb/inc/shortcode-mt-seatninja.php
+++ src/mt-seatninja-wpb/inc/shortcode-mt-seatninja.php
@@ -22,6 +22,11 @@ function mt_seatninja( $atts ) {
 
     $html = array();
 
+    $html[] = '<div class="container">';
+    $html[] = '<div class="row">';
+    $html[] = '<div class="col-xs-12 col-md-8 mt-snj-main">';
+
+
     $restaurants = mt_snj_get_restaurants();
     $html[] = '<select id="restaurants">';
 
@@ -33,8 +38,13 @@ function mt_seatninja( $atts ) {
     }
 
     $html[] = '</select>';
+    $html[] = '</div>';
+    $html[] = '<div class="col-xs-12 col-md-4 mt-snj-sidebar flex-md-first">';
+    $html[] = '</div>';
+    $html[] = '</div>';
+    $html[] = '</div>';
 
     return sprintf( '<div class="%s">%s</div>',
         trim( implode( ' ', $css_class ) ),
-        implode( '\n', $html ) );
+        implode( '', $html ) );
 }
